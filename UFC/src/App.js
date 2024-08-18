a88987dce448986eedb1d18c91c3358c2df721c2
Jidnyasa Gaikwad <136917725+Jidnyasagaikwad@users.noreply.github.com>
2024-08-19 03:35:03 +0530

    Add files via upload

--- UFC/src/App.js
+++ UFC/src/App.js
@@ -1,29 +1,25 @@
-                                                ////////// PARENT FILE OF THE PROJECT//////////////////
-
-import React from "react";
-import "./index.css";
-import Home from "./routes/Home";
-import About from "./routes/About";
-import Premier from "./routes/Premier";
-import Contact from "./routes/Contact";
-import {Route,Routes} from "react-router-dom";
-import ScrollToTop from "./ScrollToTop";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './Home';
+import Premier from './Premier';
+import About from './About';
+import Contact from './Contact';
+import ScrollToTop from './ScrollToTop'; // Ensure you import ScrollToTop
 
 function App() {
   return (
-    <>
-      {/* Routing the pages */}
-    <div className="App">
-      {/* ScrollToTop component inside App component */} 
-      <ScrollToTop />
-      <Routes>   
-        <Route path="/" element={<Home/>}  />
-        <Route path="/premier" element={<Premier/>}  />
-        <Route path="/about" element={<About/>}  />
-        <Route path="/contact" element={<Contact/>}  />
-      </Routes>
-    </div>
-    </>
+    <Router>
+      <div className="App">
+        {/* ScrollToTop component inside App component */} 
+        <ScrollToTop />
+        <Routes>   
+          <Route path="/" element={<Home />} />
+          <Route path="/premier" element={<Premier />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
