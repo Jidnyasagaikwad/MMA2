f9441361692c4740118781eb4f9ba2e768823cc8
Jidnyasa Gaikwad <136917725+Jidnyasagaikwad@users.noreply.github.com>
2024-08-18 07:21:53 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
+import PlayerCard from '../components/PlayerCard'
+import AnimatedPage from '../components/AnimatedPage'
+
+const About = () => {
+  return (
+    <AnimatedPage>
+    <div>
+      
+      <Navbar />
+      <HeroImg2 heading="Our Squad" text="Welcome to the Team."/>
+      <PlayerCard />
+      <Footer />
+    </div>
+    </AnimatedPage>
+  )
+}
+
+export default About
